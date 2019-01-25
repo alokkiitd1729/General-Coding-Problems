b22182add369efdf36ad74445c2662bdc2947641
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 05:14:23 +0530

    Create ZigZag Tree Traversal.java

---
+++ ZigZag Tree Traversal.java
@@ -0,0 +1,50 @@
+
+
+/**
+ *     Date:- 2018-12-24
+ *         Author:- @_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    Link for the problem:- https://practice.geeksforgeeks.org/problems/vertical-width-of-a-binary-tree/1
+ *    
+ *             
+ */
+
+//User function Template for Java
+
+class Node
+{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}
+
+// Print the zig zag traversal of tree.
+class GFG
+{
+   Stack<Node>s1=new Stack<>();
+   Stack<Node>s2=new Stack<>();
+	void zigZagTraversal(Node node)
+	{
+	    if(node==null){return;}
+	 s1.add(node);  
+	 while(!s1.empty()||!s2.empty()){
+	     while(!s1.empty()){
+	         Node temp=s1.pop();
+	         System.out.print(temp.data+" ");
+	         if(temp.left!=null){s2.add(temp.left);}
+	         if(temp.right!=null){s2.add(temp.right);}
+	     }
+	     while(!s2.empty()){
+	         Node temp1=s2.pop();
+	         System.out.print(temp1.data+" ");
+	         if(temp1.right!=null){s1.add(temp1.right);}
+	         if(temp1.left!=null){s1.add(temp1.left);}
+	     }
+	 }
+	}
+}
