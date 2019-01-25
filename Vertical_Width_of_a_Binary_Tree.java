5a10f8f98f9732e4519add56faa28dd54792f620
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 05:02:26 +0530

    Create Vertical_Width_of_a_Binary_Tree.java

---
+++ Vertical_Width_of_a_Binary_Tree.java
@@ -0,0 +1,34 @@
+
+
+/**
+ *     Date:- 2018-12-24
+ *         Author:- @_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    Link for the problem:- https://practice.geeksforgeeks.org/problems/vertical-width-of-a-binary-tree/1
+ 
+ */
+class Node
+{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+} 
+// Function should return vertical width of the binary tree.
+class GFG
+{
+    static void help(Node root,int h,Set<Integer> set){
+        if(root==null){return;}
+	    set.add(h);
+	    help(root.left,h-1,set);help(root.right,h+1,set);
+    }
+    public static int verticalWidth(Node root)
+	{
+	    Set<Integer> set=new HashSet<>();
+	   help(root,1,set);return set.size();
+	}
+}
