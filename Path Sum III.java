16a88be6f16111ddb5cc554c12e31df918c8deb8
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 05:37:51 +0530

    Create Path Sum III.java

---
+++ Path Sum III.java
@@ -0,0 +1,50 @@
+/**
+ *     Date:- 2018-12-24
+ *         Author:- @_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    Link for the problem:- https://leetcode.com/problems/path-sum-iii/
+ *    
+ *             
+ */
+ 
+ 
+  * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+  
+class Solution {
+     public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        helper(root, 0, sum, preSum);
+        return count;
+    }
+    int count = 0;
+    public void helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return;
+        }
+        
+        currSum += root.val;
+
+        if (preSum.containsKey(currSum - target)) {
+            count += preSum.get(currSum - target);
+        }
+        
+        if (!preSum.containsKey(currSum)) {
+            preSum.put(currSum, 1);
+        } else {
+            preSum.put(currSum, preSum.get(currSum)+1);
+        }
+        
+        helper(root.left, currSum, target, preSum);
+        helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+    }
+    
+}
