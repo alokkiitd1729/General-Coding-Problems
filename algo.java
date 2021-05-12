936fc6f47bd015981a61dbed13aaee790afd5a88
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2021-05-12 21:49:08 +0530

    Update algo.java

--- algo.java
+++ algo.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
 // long B=972663749,A=911382323;
  public class algo{
 /// functions
-  static BufferedReader s1;
+static BufferedReader s1;
 static BufferedWriter out;
 static String read() throws IOException{String line="";while(line.length()==0){line=s1.readLine();continue;}return line;}
 static int int_v (String s1){return Integer.parseInt(s1);}
@@ -521,14 +521,28 @@ static void spf(int n, int[] spf){
         }
     }
 //
-
+// find prime O(nlog(logn))
+static void mark_prime() throws IOException {
+    boolean[] b=new boolean[1000000];
+    Arrays.fill(b,true);
+    for(int i=2;i*i<=1000000;i++){
+        // note i*i<=1000000  after 1000 we are not iterating, after  
+        //that all the true marked remaining no will be prime for sure
+        if(b[i]) continue;
+        for(int j=i*i;j<=1000000;j+=i){
+            b[j]=false;
+        }
+    }
+}
 //  Binary Index Tree
+// add delta to index x
 void update(int x, int delta,int[] BIT){
        while(x<BIT.length){
             BIT[x] += delta;
            x+=x&-x;
        }
 }
+// find prefix sum upto i
 int query(int x,int[] BIT){
     int sum = 0;
         while(x>0){
@@ -674,9 +688,10 @@ static int[] rank;
  static void union_set(int x, int y){
     x=find_set(x);
     y=find_set(y);
-    if(x==y){rank[x]++;}
-    else if(rank[x]<rank[y]){int k=x;x=y;y=k;}
+    if(x==y) return;
+    if(rank[x]<rank[y]){int k=x;x=y;y=k;}
     parent[y]=x;
+    if(rank[x]==rank[y]){rank[x]++;}
  }
 
 class OrderStatisticTree{
@@ -760,6 +775,26 @@ static long[] egcd(long a, long b) {
     return (egcd(x, m)[1] + m) % m; 
   }
 
+static int modInverse(int a, int m)
+{
+    int m0 = m;
+    int y = 0, x = 1;
+
+    if (m == 1)
+        return 0;
+    while (a > 1) {
+        int q = a / m;
+        int t = m;
+        m = a % m;
+        a = t;
+        t = y;
+        y = x - q * y;
+        x = t;
+    }
+    if (x < 0){x += m0;}
+    return x;
+}
+
 /*
 
 static void usaco1() throws  IOException{
@@ -789,3 +824,162 @@ static void usaco1() throws  IOException{
 
 
 
+/*
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <utility>
+
+typedef std::pair<int, int> ii;
+
+template <class T = int>
+class Dinic {
+public:
+  struct Edge {
+    Edge(int a, T b){to = a;cap = b;}
+    int to;
+    T cap;
+  };
+
+  Dinic(int n) {
+    edges.resize(n);
+    this->n = n;
+  }
+
+  T maxFlow(int src, int sink) {
+    T ans = 0;
+    while(bfs(src, sink)) {
+      T flow;
+      pt = std::vector<int>(n, 0);
+      while(flow = dfs(src, sink)) {
+        ans += flow;
+      }
+    }
+    return ans;
+  }
+
+  void addEdge(int from, int to, T cap = 1) {
+    edges[from].push_back(list.size());
+    list.push_back(Edge(to, cap));
+    edges[to].push_back(list.size());
+    list.push_back(Edge(from, 0));
+  }
+private:
+  int n;
+  std::vector<std::vector<int> > edges;
+  std::vector<Edge> list;
+  std::vector<int> h, pt;
+
+  T dfs(int on, int sink, T flow = 1e9) {
+    if(flow == 0) {
+      return 0;
+    } if(on == sink) {
+      return flow;
+    }
+    for(; pt[on] < edges[on].size(); pt[on]++) {
+      int cur = edges[on][pt[on]];
+      if(h[on] + 1 != h[list[cur].to]) {
+        continue;
+      }
+      T got = dfs(list[cur].to, sink, std::min(flow, list[cur].cap));
+      if(got) {
+        list[cur].cap -= got;
+        list[cur ^ 1].cap += got;
+        return got;
+      }
+    }
+    return 0;
+  }
+
+  bool bfs(int src, int sink) {
+    h = std::vector<int>(n, n);
+    h[src] = 0;
+    std::queue<int> q;
+    q.push(src);
+    while(!q.empty()) {
+      int on = q.front();
+      q.pop();
+      for(auto a : edges[on]) {
+        if(list[a].cap == 0) {
+          continue;
+        }
+        int to = list[a].to;
+        if(h[to] > h[on] + 1) {
+          h[to] = h[on] + 1;
+          q.push(to);
+        }
+      }
+    }
+    return h[sink] < n;
+  }
+};
+
+std::vector<ii> divs(int n) {
+  std::vector<ii> ans;
+  for(int i = 2; i * i <= n; i++) {
+    int f = 0;
+    while(n % i == 0) {
+      n /= i;
+      f++;
+    }
+    if(f) {
+      ans.emplace_back(i, f);
+    }
+  }
+  if(n > 1) {
+    ans.emplace_back(n, 1);
+  }
+  return ans;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+  int n, m;
+  std::cin >> n >> m;
+  std::vector<int> a(n);
+  for(int i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
+  std::map<int, std::map<int, int>> freqs;
+  std::vector<ii> edges;
+  Dinic<int> base(n + 2);
+  int src = n, sink = n + 1;
+  for(int i = 0; i < m; i++) {
+    int u, v;
+    std::cin >> u >> v;
+    u--;
+    v--;
+    if(u % 2 == 1) {
+      std::swap(u, v);
+    }
+    edges.emplace_back(u, v);
+    base.addEdge(u, v, 100);
+  }
+  for(int i = 0; i < n; i++) {
+    auto v = divs(a[i]);
+    for(auto d : v) {
+      freqs[d.first][i] = d.second;
+    }
+  }
+  int ans = 0;
+  for(auto g : freqs) {
+    Dinic<int> graph = base;
+    //std::cout << "for " << g.first << std::endl;
+    for(auto f : g.second) {
+      int u = f.first;
+      int c = f.second;
+      //std::cout << "cap " << c << ", ";
+      if(u % 2 == 0) {
+        //std::cout << "adding first type for " << u << std::endl;
+        graph.addEdge(src, u, c);
+      } else {
+        //std::cout << "adding second type for " << u << std::endl;
+        graph.addEdge(u, sink, c);
+      }
+    }
+    ans += graph.maxFlow(src, sink);
+  }
+  std::cout << ans << std::endl;
+}
+*/
